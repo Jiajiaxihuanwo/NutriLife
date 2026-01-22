d97abc61bf2240ee7ef8c331377e2b325b3c4ccd
am1DAM2 <xinboliu555@gmail.com>
2026-01-22 09:50:38 +0100

    bottom menu creado

---
+++ app/src/main/java/com/xindanxin/nutrilife/main/MainActivity.java
@@ -0,0 +1,50 @@
+package com.xindanxin.nutrilife.main;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.xindanxin.nutrilife.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+
+            if (item.getItemId() == R.id.nav_dashboard) {
+//                fragment = new Dashboard();
+            } else if (item.getItemId() == R.id.nav_meals) {
+//                fragment = new Meals();
+            } else if (item.getItemId() == R.id.nav_calendar) {
+//                fragment = new Planner();
+            } else if (item.getItemId() == R.id.nav_goals) {
+//                fragment = new Goals();
+            } else if (item.getItemId() == R.id.nav_profile) {
+//                fragment = new Profile();
+            }
+
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.fragment_container, fragment)
+//                    .commit();
+
+            return true;
+        });
+
+        bottomNav.setSelectedItemId(R.id.nav_profile);
+    }
+}
