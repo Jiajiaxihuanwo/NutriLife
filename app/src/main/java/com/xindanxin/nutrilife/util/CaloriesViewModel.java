0a4b5d53b9efc84466c6fa5cf9d5e93e989cb4f8
am1DAM2 <xinboliu555@gmail.com>
2026-01-30 12:41:11 +0100

    agregado el paso de los macros al igual que solucionar pequeño bug a la hora de eliminar alimentos

--- app/src/main/java/com/xindanxin/nutrilife/util/CaloriesViewModel.java
+++ app/src/main/java/com/xindanxin/nutrilife/util/CaloriesViewModel.java
@@ -3,33 +3,45 @@ package com.xindanxin.nutrilife.util;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class CaloriesViewModel extends ViewModel {
 
-    private final MutableLiveData<Map<String, Integer>> caloriesMap =
+    private final MutableLiveData<Map<String, MacroInfo>> macrosMap =
             new MutableLiveData<>(new HashMap<>());
 
-    public LiveData<Map<String, Integer>> getCalories() {
-        return caloriesMap;
+    // Devuelve todos los macros
+    public LiveData<Map<String, MacroInfo>> getMacros() {
+        return macrosMap;
     }
 
-    public void setCalories(String mealType, int calories) {
-        Map<String, Integer> current = caloriesMap.getValue();
+    // Setea los macros de un mealType
+    public void setMacros(String mealType, int calories, int protein, int carbs, int fat) {
+        Map<String, MacroInfo> current = macrosMap.getValue();
         if (current == null) current = new HashMap<>();
 
-        current.put(mealType, calories);
-        caloriesMap.setValue(current);
+        current.put(mealType, new MacroInfo(calories, protein, carbs, fat));
+        macrosMap.setValue(current);
     }
 
-    public int getTotalCalories() {
-        int total = 0;
-        Map<String, Integer> current = caloriesMap.getValue();
+    // Obtener totales de toda la lista
+    public MacroInfo getTotalMacros() {
+        int totalCalories = 0;
+        int totalProtein = 0;
+        int totalCarbs = 0;
+        int totalFat = 0;
+
+        Map<String, MacroInfo> current = macrosMap.getValue();
         if (current != null) {
-            for (int c : current.values()) total += c;
+            for (MacroInfo m : current.values()) {
+                totalCalories += m.calories;
+                totalProtein += m.protein;
+                totalCarbs += m.carbs;
+                totalFat += m.fat;
+            }
         }
-        return total;
+
+        return new MacroInfo(totalCalories, totalProtein, totalCarbs, totalFat);
     }
 }
