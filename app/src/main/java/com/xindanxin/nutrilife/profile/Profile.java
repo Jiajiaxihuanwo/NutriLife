f7a22a99d4274caab6ab2fc6a7d31ceb9ab7b002
am2dam2 <danielvegaprada@gmail.com>
2026-02-12 12:44:02 +0100

    String_esp

--- app/src/main/java/com/xindanxin/nutrilife/profile/Profile.java
+++ app/src/main/java/com/xindanxin/nutrilife/profile/Profile.java
@@ -1,14 +1,18 @@
 package com.xindanxin.nutrilife.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.xindanxin.nutrilife.R;
+import com.xindanxin.nutrilife.auth.Login;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +65,27 @@ public class Profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view= inflater.inflate(R.layout.fragment_profile, container, false);
+        EditText etCal= view.findViewById(R.id.etDailyCal);
+        EditText etCarb= view.findViewById(R.id.etCarbsGoal);
+        EditText etFat= view.findViewById(R.id.etFatGoals);
+        EditText etProtein= view.findViewById(R.id.etProteinGoal);
+        EditText etWater= view.findViewById(R.id.etWaterGoals);
+
+        AppCompatButton btnSignOut = view.findViewById(R.id.btnSingOut);
+
+        btnSignOut.setOnClickListener(v -> {
+            Intent intent = new Intent(requireContext(), Login.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+
+        return view;
     }
+
+    public void SingOut(View v){
+        Intent intent= new Intent(getActivity(), Login.class);
+        startActivity(intent);
+    }
+
 }
