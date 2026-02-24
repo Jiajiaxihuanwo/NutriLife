7778829fa0458e7bf2e5e9894d9b1d8e84856b3a
am1DAM2 <xinboliu555@gmail.com>
2026-02-24 13:15:40 +0100

    aplicacion terminado

--- app/src/main/java/com/xindanxin/nutrilife/auth/Splash.java
+++ app/src/main/java/com/xindanxin/nutrilife/auth/Splash.java
@@ -48,6 +48,6 @@ public class Splash extends AppCompatActivity {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK).
                     addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
-        },1000);
+        },2500);
     }
 }
